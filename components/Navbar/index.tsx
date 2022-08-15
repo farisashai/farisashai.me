b60fe0d22b08030f7fc79ef43ee72f283576e085
Faris Ashai <farisashai@gmail.com>
2022-08-15 06:12:05 -0700

    Update index.tsx

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -6,6 +6,9 @@ const Navbar = () => {
     <nav className={styles.navbar}>
       <Link href="/">Home</Link>
       <div className={styles.navlinks}>
+        <Link href="https://drive.google.com/file/d/1JII5zavhs-pZSX_oZ3tWi8l0zH1v6qZR/view?usp=sharing">
+          Resume
+        </Link>
         <Link href="/projects">Projects</Link>
         <Link href="/playground">Fun</Link>
       </div>
