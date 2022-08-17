20d838fb11c9548090b67d2769e8cae16eccb6f6
Faris Ashai <farisashai@gmail.com>
2022-08-17 13:57:52 -0700

    Blob overflow hide

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,10 @@ import Navbar from 'components/Navbar';
 function MyApp({Component, pageProps}: AppProps) {
   return (
     <>
-      <div className="blob1" />
-      <div className="blob2" />
+      <div className="blobs">
+        <div className="blob1" />
+        <div className="blob2" />
+      </div>
       <Navbar />
       <Component {...pageProps} />
     </>
