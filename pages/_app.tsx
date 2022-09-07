2dfec0c26f9f43e2c5c4c7fae39808feff4ab359
Faris Ashai <farisashai@gmail.com>
2022-09-06 17:09:43 -0700

    Google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import 'styles/globals.scss';
 import type {AppProps} from 'next/app';
 import Navbar from 'components/Navbar';
+import {GoogleAnalytics} from 'nextjs-google-analytics';
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
     <>
+      <GoogleAnalytics trackPageViews />
       <Navbar />
       <Component {...pageProps} />
       <div className="blobs">
