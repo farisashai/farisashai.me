7ef5d39d065c0ffe34685ffdf55d9dfebae06ff9
Faris Ashai <farisashai@gmail.com>
2022-11-06 00:50:42 -0700

    Added page transition effect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import 'styles/globals.scss';
 import type {AppProps} from 'next/app';
 import Navbar from 'components/Navbar';
 import {GoogleAnalytics} from 'nextjs-google-analytics';
+import PageTransition from 'components/PageTransition';
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
     <>
       <GoogleAnalytics trackPageViews />
       <Navbar />
-      <Component {...pageProps} />
+      <PageTransition>
+        <Component {...pageProps} />
+      </PageTransition>
       <div className="blobs">
         <div className="blob1" />
         <div className="blob2" />
