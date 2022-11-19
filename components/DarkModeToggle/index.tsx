6412cd3fa53609de3ca2a789caa9b0adb373378a
Faris Ashai <farisashai@gmail.com>
2022-11-19 00:17:50 -0800

    Fix dark mode flash

--- components/DarkModeToggle/index.tsx
+++ components/DarkModeToggle/index.tsx
@@ -1,46 +1,25 @@
-import {useEffect, useState} from 'react';
+import {useTheme} from 'next-themes';
 import {FiSun, FiMoon} from 'react-icons/fi';
 import style from './style.module.scss';
 
 const DarkModeToggle = () => {
-  const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    const currentTheme = window.localStorage.getItem('theme');
-    if (currentTheme == 'dark') {
-      document.body.classList.toggle('dark-theme', true);
-    } else if (currentTheme == 'light') {
-      document.body.classList.toggle('light-theme', true);
-    }
-  }, []);
+  const {resolvedTheme, setTheme} = useTheme();
+  const darkMode = resolvedTheme == 'dark';
 
   return (
     <button
       className={style.button}
       type="button"
       aria-label="Dark Mode Toggle"
-      onClick={() => {
-        const prefersDarkScheme =
-          window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)');
-        if (prefersDarkScheme.matches) {
-          document.body.classList.toggle('light-theme');
-          var theme = document.body.classList.contains('light-theme') ? 'light' : 'dark';
-        } else {
-          document.body.classList.toggle('dark-theme');
-          var theme = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
-        }
-        window.localStorage.setItem('theme', theme);
-
-        setDarkMode(d => !d);
-      }}
+      onClick={() => setTheme(darkMode ? 'light' : 'dark')}
     >
       <FiSun
         size={20}
-        className={`${style.icon} ${!darkMode ? style.active : ''}`}
+        className={`${style.icon} ${darkMode ? style.active : ''}`}
       />
       <FiMoon
         size={20}
-        className={`${style.icon} ${darkMode ? style.active : ''}`}
+        className={`${style.icon} ${!darkMode ? style.active : ''}`}
       />
     </button>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,12 @@ import type {AppProps} from 'next/app';
 import Navbar from 'components/Navbar';
 import {GoogleAnalytics} from 'nextjs-google-analytics';
 import PageTransition from 'components/PageTransition';
+import {ThemeProvider} from 'next-themes';
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
-    <>
-      <GoogleAnalytics trackPageViews />
+    <ThemeProvider>
+      {process.env.NODE_ENV == 'production' ? <GoogleAnalytics trackPageViews /> : null}
       <Navbar />
       {/* <PageTransition> */}
       <Component {...pageProps} />
@@ -16,7 +17,7 @@ function MyApp({Component, pageProps}: AppProps) {
         <div className="blob1" />
         <div className="blob2" />
       </div>
-    </>
+    </ThemeProvider>
   );
 }
 
