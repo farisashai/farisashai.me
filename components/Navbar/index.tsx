25fb7a6fb69567be261367eeb6ccf6c2de15aa36
Faris Ashai <farisashai@gmail.com>
2022-11-24 02:58:25 -0800

    Now Playing Track

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,36 +3,81 @@ import Link from 'next/link';
 import styles from './styles.module.scss';
 import {useRouter} from 'next/router';
 import Image from 'next/image';
+import {Popover, Transition} from '@headlessui/react';
+import {fetcher} from 'utils';
+import {NowPlayingResponse} from 'types';
+import useSWR from 'swr';
+import {FaSpotify} from 'react-icons/fa';
 
 const Navbar = () => {
   const router = useRouter();
+
+  const {data: nowPlaying, error: nowPlayingError} = useSWR<NowPlayingResponse>(
+    '/api/nowPlaying',
+    fetcher
+  );
+
+  const activeTrack = !nowPlayingError && nowPlaying && nowPlaying?.isPlaying == true;
+
+  if (activeTrack) {
+    console.log(nowPlaying);
+  }
+
   return (
-    <>
-      <nav className={styles.navbar}>
-        <Link href="/">
-          <a className={styles.logo}>
-            <Image
-              src="/logo.svg"
-              width="32"
-              height="32"
-              alt="logo"
-            />
-          </a>
-        </Link>
-        <Link href="/resume.pdf">Resume</Link>
-        <Link href="/projects">
-          <a className={router.route === '/projects' ? styles.active : ''}>Projects</a>
-        </Link>
-        <Link href="/playground">
-          <a className={router.route === '/playground' ? styles.active : ''}>Fun</a>
-        </Link>
-        <DarkModeToggle />
-      </nav>
-      {/* <Image
-        src={'https://api.unsplash.com/random'}
-        alt=""
-      /> */}
-    </>
+    <nav className={styles.navbar}>
+      <Link href="/">
+        <a className={styles.logo}>
+          <Image
+            src="/logo.svg"
+            width="32"
+            height="32"
+            alt="logo"
+          />
+        </a>
+      </Link>
+      <Popover className={`${styles.pop} ${activeTrack ? '' : styles.hidden}`}>
+        <Popover.Button className={styles.popButton} />
+        <Popover.Panel className={styles.popPanel}>
+          <Image
+            width={152}
+            height={152}
+            src={nowPlaying?.albumImageUrl}
+            className={styles.popAlbumCover}
+            alt={nowPlaying?.title}
+          />
+          <div className={styles.popTextContent}>
+            <p className={styles.currently}>I&apos;m Currently Playing ...</p>
+            <p className={styles.title}>{nowPlaying?.title}</p>
+            <a className={styles.artist}>{nowPlaying?.artist}</a>
+            <a
+              href={nowPlaying?.songUrl}
+              className={styles.listenNow}
+            >
+              <FaSpotify size={16} />
+              Listen Now
+            </a>
+          </div>
+          {/* <h2>{nowPlaying?.title}</h2>
+          <p>{nowPlaying?.artist}</p>
+          <a href={nowPlaying?.songUrl}>Listen Now</a> */}
+        </Popover.Panel>
+      </Popover>
+      <Link href="/resume.pdf">
+        <a
+          href="replace"
+          className={styles.firstLink}
+        >
+          Resume
+        </a>
+      </Link>
+      <Link href="/projects">
+        <a className={router.route === '/projects' ? styles.active : ''}>Projects</a>
+      </Link>
+      <Link href="/playground">
+        <a className={router.route === '/playground' ? styles.active : ''}>Fun</a>
+      </Link>
+      <DarkModeToggle />
+    </nav>
   );
 };
 
--- components/Navbar/styles.module.scss.d.ts
+++ components/Navbar/styles.module.scss.d.ts
@@ -3,9 +3,20 @@
 declare namespace StylesModuleScssNamespace {
   export interface IStylesModuleScss {
     active: string;
+    artist: string;
+    currently: string;
     fadeIn: string;
+    firstLink: string;
+    hidden: string;
+    listenNow: string;
     logo: string;
     navbar: string;
+    pop: string;
+    popAlbumCover: string;
+    popButton: string;
+    popPanel: string;
+    popTextContent: string;
+    title: string;
   }
 }
 
