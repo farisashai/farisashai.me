d20f1fe5641d89f7218fa3b47e5336f4c7d73131
Faris Ashai <farisashai@gmail.com>
2023-09-28 23:13:34 -0700

    Update index.tsx

--- components/MySEO/index.tsx
+++ components/MySEO/index.tsx
@@ -17,6 +17,11 @@ const MySEO = ({page, description, path}: MySEOProps) => (
       url: `https://www.farisashai.me${path}`,
       title: `Faris Ashai | ${page}`,
       description: description,
+      images: [
+        {
+          url: 'https://media.discordapp.net/attachments/800962580041433088/1157198139221102592/IMG_1157.png?ex=6517bc40&is=65166ac0&hm=b3cfd9cb719ed22351220c9c3b8c441d7ddcfdbdadd5e16f55f6e219cf2643e4&=',
+        },
+      ],
       site_name: 'Faris Ashai',
     }}
   />
